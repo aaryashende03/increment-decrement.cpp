fde16e406f65db7ff4a59868549e742f089e1fc6
aaryashende03 <168561498+aaryashende03@users.noreply.github.com>
2024-09-13 20:27:17 +0530

    Add files via upload

---
+++ incredecre.2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int i =10 , j = 10;
+int x , y;
+
+x = i++ ; //Post increment 
+y = ++j;  // Pre increment
+
+cout<<x<<"\n";
+cout<<y<<"\n";
+
+cout<<i<<"\n";
+cout<<j<<"\n";
+
+return 0;
+}	
+
+//Post increment  -> it means that first assign the orignal value and then increment the value
+
+// Pre increment  ->  it means that first increment the value then assign the incremented value 
